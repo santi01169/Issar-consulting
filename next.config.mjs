a8388cd3e7e786b5ca95ed74fb61cf87f45cac85
santiago briceño <kspulido2005@gmail.com>
2026-04-07 12:21:09 -0500

    fix: add robots.txt

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true, // 🔥 IMPORTANTE
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
-  }
-}
+  },
+};
 
-export default nextConfig
+export default nextConfig;
